bf4b81d60ef1819229789b41d1beb2d7673c86f8
sepante <sinasajjadi@ymail.com>
2017-10-14 02:45:33 +0330

    made us of print_graph.

--- SIR.cpp
+++ SIR.cpp
@@ -252,5 +252,6 @@ int main()
 	clock_t end = clock();
   double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 	std::cout << "time: " << elapsed_secs << '\n';
-//cout<<cnct_prob<<endl;
+	//cout<<cnct_prob<<endl;
+	//boost::print_graph(society);
 }
