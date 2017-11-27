641fd9990c4f48536669a84f4e81007cdd53a067
sepante <sinasajjadi@ymail.com>
2017-11-27 05:33:16 +0330

    cleaning up some obsolete lines.

--- Src/Phase Plot.py
+++ Src/Phase Plot.py
@@ -22,18 +22,17 @@ for qindex in range(q_size):
         
         single_cluster = current_data[ np.logical_not(joint_condition) , 0]
         plt.plot([prange[pindex]], [single_cluster],'o' , color='b', alpha=opacity_num )
-        #data_type = " $Primary$ $School: $"
+
         dis_type = dis_type.replace('\n','')
         data_type = data_type.replace('\n','')
         #name_string = data_type +"$Phase: $"+" $n=$"+str(n)+ ", $q=$" + str(qrange[qindex]) + ", $r=$" + str(rrange[0])
-        name_string = dis_type + ", " + data_type +", $Phase$: $n=$"+str(n)+ ", $q=$" + str(qrange[qindex]) + ", $r=$" + str(rrange[0])
+        name_string = dis_type + ", " + data_type +", $Phase$: $n=$"+str(n)+ ", $q=$" + str(qrange[qindex]) + ", $r=$" + str(rrange[rindex])
         plt.suptitle(name_string)
         plt.xlabel('$p$')   
         plt.ylabel('$R$')
         #plt.xlim([0.03,0.1])
         plt.ylim([0-0.02,1+0.02])
         #plt.ylim([0.2, 1])
-        #plt.ylim([0.8,1+0.02])*=
         name_string = name_string.replace('$','')
         
         
--- Src/data_reader.py
+++ Src/data_reader.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 with open('cdata.txt') as f:
-    #for i in range(1):
     dis_type=(f.readline())
     data_type=(f.readline())
     
@@ -25,34 +24,3 @@ with open('cdata.txt') as f:
 data = np.array(pd_data)
 data = np.array(data).reshape(p_size, q_size, runNum,3)
 nindex = rindex = pindex = 0
-    #print(f)
-"""
-opacity_num =  500/runNum
-opacity_num =  1
-#cmap = mpl.cm.rainbow
-for qindex in range(q_size):
-    q=qrange[qindex]
-    n = nrange[0]
-    #for run in range(runNum):
-        #plt.plot(prange, data[:,qindex,run],'o' , color='g', alpha=opacity_num )
-    plt.plot(prange, [data],'o' , color='g', alpha=opacity_num )
-
-    #data_type = " $Primary$ $School: $"
-    dis_type = dis_type.replace('\n','')
-    data_type = data_type.replace('\n','')
-    #name_string = data_type +"$Phase: $"+" $n=$"+str(n)+ ", $q=$" + str(qrange[qindex]) + ", $r=$" + str(rrange[0])
-    name_string = dis_type + ", " + data_type +", $Phase$: $n=$"+str(n)+ ", $q=$" + str(qrange[qindex]) + ", $r=$" + str(rrange[0])
-    plt.suptitle(name_string)
-    plt.xlabel('$p$')   
-    plt.ylabel('$R$')
-    #plt.xlim([0.03,0.1])
-    #plt.ylim([0-0.02,1+0.02])
-    #plt.ylim([0.2, 1])
-    #plt.ylim([0.8,1+0.02])*=
-    name_string = name_string.replace('$','')
-    
-    
-    location = "results/"
-    #plt.savefig(location+name_string+".png")
-    plt.show()
-"""
