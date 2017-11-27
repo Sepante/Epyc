7a308e758dadcb4003041c9f06009ea9c157c489
sepante <sinasajjadi@ymail.com>
2017-11-28 01:08:00 +0330

    matrixplot is completed. the outputs are pictures and are later turned to gif.

--- Src/matrixplot.py
+++ Src/matrixplot.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
-import pandas as pd    
+import pandas as pd
 from pylab import *
+
 
 
 with open('grid_visualize.csv') as f:
@@ -13,11 +15,19 @@ l = len(vis_data[0])
 t = int(len(vis_data) / l)
 
 vis_data = np.array(vis_data).reshape(t, l, l)
+bounds = np.array([0, 1, 2, 3, 4, 6, 9, 12, 18, 36]) + 0.1
+cmap = mpl.colors.ListedColormap(['royalblue', 'pink', 'pink' ,'orange', 'red', 'yellow', 'pink', 'pink', 'green'])
+norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
 
-#imshow(A, interpolation='nearest')
-
-for time_step in vis_data:
-    #plt.matshow(time_step)
-    imshow(time_step, interpolation='nearest')
+images = []
+location = "results/grid_animation/"
+for t, anim_step in enumerate(vis_data):
+    imshow(anim_step, cmap = cmap, norm = norm, interpolation='nearest')
     #grid(True)
-    plt.show()
+    name_string = '{:003d}'.format(t)
+    plt.suptitle(name_string)
+    if t%5 == 4:
+        plt.savefig(location+name_string+".png")
+    
+    #plt.show(fig)
+    #plt.show()
