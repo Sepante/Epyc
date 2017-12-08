b66db9abf222f7a3cb23d9b71b1cd2feb6657d72
sepante <sinasajjadi@ymail.com>
2017-12-08 21:17:58 +0330

    folder name changed to remove space from the name.

--- Graph_data/data_cleaner.py
+++ Graph_data/data_cleaner.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+#from collections import Counter
 
 #lines = open('tij_InVS.dat').read().splitlines()
 lines = open('primaryschool.csv').read().splitlines()
@@ -22,6 +23,7 @@ output = open('clean_input_matrix.txt', 'w')
 """ this data has a very big gap, this line fixes deletes the gap."""
 data[60623:,0] -= 54920
 #data[100:,0] += 100
+
 """ this data is short we double it's period by repeating the edges."""
 data[len(lines):] = data[:len(lines)]
 jump = data[len(lines),0] - data[len(lines)-1,0]
@@ -39,3 +41,16 @@ non_zeros_ind = q != 0
 
 q = q[non_zeros_ind]
 print(min(q))
+edge_count = pd.Series(data[:, 0])
+edge_count = edge_count.value_counts()
+edge_count = edge_count.sort_index()
+edge_count = np.array(edge_count.reset_index())
+c_grain_size = 200
+splitted_edge = np.array_split(edge_count[:,1],c_grain_size)
+splitted_time = np.array_split(edge_count[:,0],c_grain_size)
+c_grained_count_edge = [np.mean(inst) for inst in splitted_edge]
+c_grained_count_time = [np.mean(inst) for inst in splitted_time]
+#c_grained_count_x = np.mean(np.array_split(edge_count[:,0],c_grain_size),1)
+
+#plt.plot(edge_count[::20,0],edge_count[::20,1],'-o')
+plt.plot(c_grained_count_time, c_grained_count_edge, '-o')
