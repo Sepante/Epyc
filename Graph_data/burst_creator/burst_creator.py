cb96268700d57f2a1930e60f23b58717be8bfcb4
sepante <sinasajjadi@ymail.com>
2018-01-29 21:02:23 +0330

    burst_creator creates bursy data. currrently only lognormal.

---
+++ Graph_data/burst_creator/burst_creator.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+from scipy.stats import expon
+import scipy.stats as st
+import pandas as pd
+
+N = 500000
+graph_size = 256
+#N = 1000
+ld = 0.2
+#cnct = expon.rvs(size=N, loc = 0.1, scale = 0.9)
+#cnct = expon.rvs(size=N, loc = 1, scale = 0)
+#cnct = np.random.poisson(ld, N)/ld
+#cnct = (np.random.binomial(n, p, N)/(n*p))
+cnct = np.random.lognormal( np.log(np.e), 0, N)
+#cnct = st.lognorm.rvs( 1, 1 , size=N )
+#cnct = st.powerlaw.rvs( 100000, -1, size=N )
+#cnct = np.random.lognormal(np.log10(10000),2,N)
+cnct = np.round(cnct,0)
+cnct /= cnct.mean()
+
+
+graph = np.zeros((N, 3), int)
+
+graph[:, 0] = np.cumsum(cnct)*(10)
+graph[:, 0] -= graph[0, 0]
+
+"""
+plt.plot(graph[:, 0], np.ones(N),'o',alpha = 20/N)
+#plt.plot(cnct,'o')
+#plt.xlim([0,N])
+#plt.show()
+
+print (cnct.min())
+print (np.mean(cnct))
+print (np.std(cnct))
+#print('\n')
+
+print("burst = ",burst)
+"""
+cnct = np.diff(graph[:,0])
+burst = ( np.std(cnct) - np.mean(cnct) ) / (np.std(cnct) + np.mean(cnct) )
+graph[:, 1:] = np.random.randint(0,graph_size,[N,2])
+
+
+output = open('burst_graph.txt', 'w')
+#output.write("salam")
+t_respect_graph = pd.DataFrame(graph)
+t_respect_graph.to_csv('burst_graph.txt',index = False, header = False, sep = '\t') 
