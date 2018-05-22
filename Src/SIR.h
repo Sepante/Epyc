50715d73a4ed557466cfa84c8b03c65d56a891cc
sepante <sinasajjadi@protonmail.com>
2018-05-22 12:57:04 +0430

    typedef enum changed to enum.

--- Src/SIR.h
+++ Src/SIR.h
@@ -2,15 +2,14 @@
 #define SIR_H
 #include <iostream>
 
-typedef enum { neither = 1, dis_one = 2 , dis_two = 3, both = 6 } Transfer;
+enum Transfer { neither = 1, dis_one = 2 , dis_two = 3, both = 6 };
 class SIR
 {
 	private:
 	int health = 1;
 	int future = 1;
+
 	public:
-
-
 	void set_health(int s);
 	void set_future(int s);
 
@@ -18,7 +17,6 @@ class SIR
 
 	int get_health();
 	int get_future();
-
 
 	Transfer demand();
 	Transfer supply();
