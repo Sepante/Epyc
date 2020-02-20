478efd5079a809964678af5295e891c7be52a2e6
sepante <sinasajjadi@protonmail.com>
2020-02-20 22:45:28 +0330

    nor-binned_Plot.py now recognizes normalized vs non-normalized .

--- data analysis/norm-binned_Plot.py
+++ data analysis/norm-binned_Plot.py
@@ -9,23 +9,26 @@ matplotlib.rcParams.update({'font.size': 15})
 #from data_reader import datareader
 #import data_reader
 n = nrange[nindex]
+node_number_normalized = True
 
 name_string = "$normal$, " + dis_type + ", " + data_type + ", $n=$" + str(nrange[nindex]) + ", $p=$" + str(prange[pindex]) + ", $q=$" + str(qrange[qindex]) + ", $r=$" + str(rrange[rindex])
 #n=243
 opacity_num = 0.5
 
 fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
+#ax = fig.add_subplot(1,1,1)
 
 #for pindex in range(p_size):
+for pindex in [-1]:
+    p = prange[pindex]
     #for qindex in range(q_size):
-for pindex in [0, 10, 20, 30, 40, 50, 59]:
+#for pindex in [0, 10, 20, 30, 40, 50, 59]:
     for qindex in [-1]:
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
         
         
-        current_data = data[pindex, qindex, :]
+        #current_data = data[pindex, qindex, :]
         
         #high_a = current_data[:, 1] > -1
         #high_b = current_data[:, 2] > -1
@@ -34,17 +37,26 @@ for pindex in [0, 10, 20, 30, 40, 50, 59]:
         #joint_cluster = np.sum(current_data[joint_condition, :],1)
         #joint_cluster = np.sum( data[pindex, qindex, :] , 1)
         joint_cluster = data[pindex, qindex, :, 0]
+        #joint_cluster = data[pindex, qindex, :, 1]
 
         #joint_cluster = joint_cluster[::2]
         #joint_cluster = joint_cluster[y_pred == 0]
 
+            #cs = ax.imshow(im1, interpolation='none', origin = 'lower', aspect = aspect , cmap=plt.cm.gnuplot , extent =[prange[0],prange[-1],  1 + omitted_bins * binLen ,  n ] )
 
-        hist, bins = np.histogram(joint_cluster, int(n/1), range = (0, n))
+        hist, bins = np.histogram(joint_cluster, int(n), range = (0, n))
+        
         widths = np.diff(bins)
         hist = hist / (runNum)
         hist = hist / widths[0]
-        #plt.bar(bins[:-1], hist, widths,  color = 'orange', alpha = 1, ec='black')
-        plt.fill(hist, bins[:-1], color = 'orange')
+
+        if node_number_normalized:
+            bins /= n
+
+        #ax.bar(bins[:-1], hist, widths,  color = 'orange', alpha = 1, ec='black')
+        ax.fill(hist, bins[:-1], color = 'orange')
+        #plt.xlim([20/n,500/n])
+        #plt.ylim([0, 2])
         #plt.show()
         
         
@@ -83,8 +95,10 @@ for pindex in [0, 10, 20, 30, 40, 50, 59]:
 
         #ax.set_aspect('equal')9
         #plt.ylim([0,0.06])
-        #plt.xlim([10,n])
-        ax.set_ylim([17, n])
+        #plt.xlim([0, 0.01])
+
+
+        ax.set_ylim([0.2133*1, 1])
         ax.set_xlim([0, 0.015])
         ax.set_aspect(1/ax.get_data_ratio())
         
@@ -92,8 +106,8 @@ for pindex in [0, 10, 20, 30, 40, 50, 59]:
         #plt.savefig(location+name_string+".png" , bbox_inches='tight')
         plt.show()
 
-pd_data = pd.DataFrame()
-pd_data[data_type] = hist
+#pd_data = pd.DataFrame()
+#pd_data[data_type] = hist
 #pd_data.to_csv( location + "hist compare/" + name_string+".hist", header = True , index = None , sep = ',' )
 #print(data_type)
 
